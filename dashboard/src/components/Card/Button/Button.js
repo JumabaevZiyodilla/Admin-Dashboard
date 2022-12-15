571beb4606b83173a30b1a6e898b3fec771afc2f
Ziyodilla <ziyodillajz@gmail.com>
2022-12-15 11:23:21 +0500

    Done

--- dashboard/src/components/Card/Button/Button.js
+++ dashboard/src/components/Card/Button/Button.js
@@ -1,6 +1,3 @@
-import "../card.css"
-import "./button.css"
-
 export const Button = (props) => {
     return <button {...props}>{props.children}</button>
 }
--- dashboard/src/components/Card/Card.js
+++ dashboard/src/components/Card/Card.js
@@ -1,15 +1,49 @@
 import { Button } from './Button'
-import "./card.css"
-import { Search } from '../../assets/images/icons'
+import './card.css'
+import { Search, Notification } from '../../assets/images/icons'
+// import Jones from "../../assets/images/jones.png"
 
 export const Card = () => {
+  const cardInfo = [
+    {
+      name: 'Unresolved',
+      id: '60',
+    },
+    {
+      name: 'Overdue',
+      id: '16',
+    },
+    {
+      name: 'Open',
+      id: '43',
+    },
+    {
+      name: 'On hold',
+      id: '64',
+    },
+  ]
   return (
     <section className="card">
-        <div className="card-box">
-          <p className='card-text'>Overview</p>
-          <Button classname="search-btn"><Search/></Button>
-          <Button classname="notification-btn" />
+      <div className="card-box">
+        <p className="card-text">Overview</p>
+        <div className="card-btn-box">
+          <Button className="search-btn">
+            <Search />
+          </Button>
+          <Button className="notification-btn">
+            <Notification />
+          </Button>
+          <p className="card-username">Jones Ferdinand</p>
         </div>
+      </div>
+      <ul className="card-list">
+          {cardInfo.map((element) => (
+            <li className="card-item">
+                <p className="card-item-title">{element.name}</p>
+                <p className="card-item-number">{element.id}</p>
+            </li>
+          ))}
+        </ul>
     </section>
   )
 }
